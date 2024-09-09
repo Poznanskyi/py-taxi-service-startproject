e51328ec16c12190f59d0e175d8a1d067fa8aa18
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-09-09 09:40:31 +0300

    Add .gitignore to exclude unnecessary files

---
+++ taxi/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    country = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Driver(AbstractUser):
+    license_number = models.CharField(max_length=20, unique=True)
+
+    class Meta:
+        verbose_name = "driver"
+        verbose_name_plural = "drivers"
+
+    def __str__(self):
+        return self.username
+
+
+class Car(models.Model):
+    model = models.CharField(max_length=100)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE,
+        related_name="cars"
+    )
+    drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    def __str__(self):
+        return f"{self.manufacturer.name} {self.model}"
